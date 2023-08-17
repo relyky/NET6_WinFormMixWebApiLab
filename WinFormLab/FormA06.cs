121b3e03c2620dd37340ac8d99980541751a8768
Rely Ky <relyky@hotmail.com>
2023-08-17 18:28:15 +0800

    RDLC 報表 commit

--- WinFormLab/FormA06.cs
+++ WinFormLab/FormA06.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Reporting.WinForms;
+using Microsoft.ReportingServices.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,9 +9,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinFormLab.Models;
+using WinFormLab.ReportDefinitions;
 
 namespace WinFormLab;
 
+/// <remarks>
+/// 參考：[ReportViewer Core](https://github.com/lkosson/reportviewercore)
+/// </remarks>
 public partial class FormA06 : Form
 {
   public FormA06()
@@ -19,7 +26,16 @@ public partial class FormA06 : Form
 
   private void FormA06_Load(object sender, EventArgs e)
   {      
+    //# 備好報表資料
+    ReportDataSet.ReportSampleDataTable data = new ReportDataSet.ReportSampleDataTable();
+    data.AddReportSampleRow(1, "Foo", 1001);
+    data.AddReportSampleRow(2, "Bar", 3003);
+    data.AddReportSampleRow(3, "Baz", 6006);
+
+    //# 填入報表
     reportViewer1.LocalReport.ReportEmbeddedResource = "WinFormLab.ReportDefinitions.HelloWorldReport.rdlc";
-    reportViewer1.RefreshReport();
+    reportViewer1.LocalReport.DataSources.Add(new ReportDataSource("ReportData", (DataTable)data));
+    reportViewer1.LocalReport.SetParameters(new[] { new ReportParameter("ReportParameter1", "我來自程式填入的參數值。") });
+    reportViewer1.RefreshReport(); // 刷新報表
   }
 }
