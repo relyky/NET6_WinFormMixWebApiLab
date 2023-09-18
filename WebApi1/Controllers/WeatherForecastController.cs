84c416d281b85ac153069391c97a1d22f2a5c349
Rely Ky <relyky@hotmail.com>
2023-09-18 14:06:43 +0800

    Authorize test

--- WebApi1/Controllers/WeatherForecastController.cs
+++ WebApi1/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using CommonRes;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WebApi1.Controllers;
 
@@ -20,6 +21,7 @@ public class WeatherForecastController : ControllerBase
   }
 
   [HttpGet(Name = "GetWeatherForecast")]
+  [Authorize]
   public IEnumerable<WeatherForecast> Get()
   {
     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
