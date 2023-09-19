edf90652c57f0b6d48dac0343c2f20ab013cd84d
Rely Ky <relyky@hotmail.com>
2023-09-19 17:06:13 +0800

    子系統－登入驗證

--- DemoSysAPI/Program.cs
+++ DemoSysAPI/Program.cs
@@ -1,15 +1,52 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//§§ Add services to the container. ===========================================
 
-builder.Services.AddControllers();
+//## for Authentication & Authorization
+builder.Services.AddAuthentication(option =>
+{
+  option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+  option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+  option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(option =>
+{
+  option.TokenValidationParameters = new TokenValidationParameters
+  {
+    ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+    ValidAudience = builder.Configuration["JwtSettings:Audience"],
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["JwtSettings:SigningKey"])),
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidateLifetime = true,
+    ValidateIssuerSigningKey = true,
+  };
+});
+//builder.Services.AddAuthorization(option =>
+//{
+//  option.AddPolicy(IdentityAttr.AdminPolicyName, p =>
+//    p.RequireClaim(IdentityAttr.AdminClaimName, "true"));
+//});
+
+builder.Services.AddControllers(opt =>  // or AddMvc()
+{
+  //## 禁止自動把 HTTP Response 為 null 時轉換成 204 NoContent。因為 System.Text.Json 無法對 204 NoContent 解序列化！
+  // 此設定應該只適用於 Web API。
+  // remove formatter that turns nulls into 204 - No Content responses
+  opt.OutputFormatters.RemoveType<HttpNoContentOutputFormatter>();
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//§§ Configure the HTTP request pipeline. =====================================
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
@@ -18,6 +55,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//## for Authentication & Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- DemoSysUI/RefitClient/MyValueApi.cs
+++ DemoSysUI/RefitClient/MyValueApi.cs
@@ -8,15 +8,15 @@ public interface IMyValueApi
   [Get("/api/MyValue")]
   Task<List<MyValue>> QryValuesAsync();
 
-  [Post("/api/MyValue")]
-  Task<MyValue> AddValueAsync(MyValue formData);
+  //[Post("/api/MyValue")]
+  //Task<MyValue> AddValueAsync(MyValue formData);
 
-  [Get("/api/MyValue/{id}")]
-  Task<MyValue?> GetValueAsync([AliasAs("id")] int Id);
+  //[Get("/api/MyValue/{id}")]
+  //Task<MyValue?> GetValueAsync([AliasAs("id")] int Id);
 
-  [Put("/api/MyValue")]
-  Task<MyValue?> UpdValueAsync(MyValue formData);
+  //[Put("/api/MyValue")]
+  //Task<MyValue?> UpdValueAsync(MyValue formData);
 
-  [Delete("/api/MyValue/{id}")]
-  Task<int> DelValueAsync(int id);
+  //[Delete("/api/MyValue/{id}")]
+  //Task<int> DelValueAsync(int id);
 }
